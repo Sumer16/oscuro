4e0b7798d0117d31c60dcec8a608892ea079513f
Sumer Patel <sumerpatel.dev@gmail.com>
2024-10-30 11:27:33 -0700

    Organizing code

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,4 +1,6 @@
 import { Link, Stack } from 'expo-router';
+
+import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
---
+++ components/OscuroButton.tsx
@@ -0,0 +1,68 @@
+import { Ionicons } from '@expo/vector-icons';
+
+import { ComponentProps } from 'react';
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
+
+import { Colors } from '@/constants/Colors';
+
+interface OscuroButtonProps {
+  onPress: () => void;
+  title?: string;
+  iconName?: ComponentProps<typeof Ionicons>['name'];
+  containerStyle?: StyleProp<ViewStyle>;
+  iconSize?: number;
+}
+
+export default function OscuroButton({
+  onPress,
+  iconName,
+  title,
+  containerStyle,
+  iconSize,
+}: OscuroButtonProps) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        styles.container,
+        {
+          backgroundColor: Colors.dark.background,
+          borderRadius: title ? 6 : 40,
+          alignSelf: 'flex-start',
+        },
+        containerStyle,
+      ]}
+    >
+      {iconName && (
+        <Ionicons name={iconName} size={iconSize ?? 28} color={'white'} />
+      )}
+      {title ? (
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '600',
+            color: 'white',
+          }}
+        >
+          {title}
+        </Text>
+      ) : null}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 7,
+    borderRadius: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 7,
+  },
+});
