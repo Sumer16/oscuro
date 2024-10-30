588465b37a43a78eaeef37ff8c7198313e5f5fd2
Sumer Patel <sumerpatel.dev@gmail.com>
2024-10-30 13:47:01 -0700

    Turn off video

--- app/index.tsx
+++ app/index.tsx
@@ -73,7 +73,7 @@ export default function HomeScreen() {
             torch={torch}
             zoom={zoom}
             photo={true}
-            video={true}
+            // video={true}
           />
           <BlurView
             intensity={100}
