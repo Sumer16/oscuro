3e9fb65387f4526f732b07704ceddfce42081e8c
Sumer Patel <sumerpatel.dev@gmail.com>
2024-10-30 11:16:08 -0700

    Added camera to main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import { Redirect, useRouter } from 'expo-router';
 
 import React from 'react';
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, StatusBar, SafeAreaView } from 'react-native';
 import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   const router  = useRouter();
@@ -17,13 +16,42 @@ export default function HomeScreen() {
 
   if (redirectToPermissions) return <Redirect href={'/permissions'} />;
 
-  if (!device) return <></>
+  if (!device) return <></>;
 
   return (
-    <ThemedText type='title'>Hello World!</ThemedText>
+    <>
+      <SafeAreaView style={styles.container}>
+        {/* Camera view */}
+        <View style={{ flex: 2, borderRadius: 10, overflow: 'hidden' }}>
+          <Camera
+            style={{ flex: 1 }}
+            device={device}
+            isActive
+          />
+        </View>
+
+        {/* Camera Controls */}
+        <View style={{ flex: 1 }}>
+          {/* Top section */}
+          <View style={{ flex: 0.7 }}>
+            <ThemedText>Max FPS: {device.formats[0].maxFps}</ThemedText>
+            <ThemedText>Width: {device.formats[0].photoWidth} Height: {device.formats[0].photoHeight}</ThemedText>
+            <ThemedText>Camera: {device.name}</ThemedText>
+          </View>
+          
+          {/* Bottom section */}
+          <View style={{ flex: 0.7 }}>
+
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
 });
